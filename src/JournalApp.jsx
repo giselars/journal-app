86f5b6257ece255354a53ced7a9c2704f1c2f464
giselars <gisela-rs@outlook.com>
2022-12-07 13:35:11 +0100

    :wrench: update configuration files

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,13 +1,11 @@
-import { BrowserRouter } from "react-router-dom"
-import { AppRouter } from "./router/AppRouter"
+import { AppRouter } from "./router/AppRouter";
+import { AppTheme } from "./theme";
 
 
 export const JournalApp = () => {
   return (
-    <>
-      <BrowserRouter>
+    <AppTheme>
         <AppRouter/>
-      </BrowserRouter>
-    </>
+    </AppTheme>
   )
 }
